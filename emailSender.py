6d66e8a12438aac96e5eeafc5af9e3056cdcefb6
Valentina Banner <78287030+realhuman101@users.noreply.github.com>
2024-11-28 10:39:15 +0800

    make QR codes + start working on emails

--- emailSender.py
+++ emailSender.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.by import By
 from selenium import webdriver
 
 import os
+from time import sleep
 
 EMAIL = os.environ.get('EMAIL')
 USERNAME = os.environ.get('USERNAME')
@@ -14,6 +15,26 @@ driver = webdriver.Firefox()
 driver.get('https://mail.google.com/mail/u/0/#inbox')
 
 # Sign in
-enterAddressField = driver.find_element(By.XPATH, '//*[@id="identifierId"]')
+driver.find_element(By.XPATH, '//*[@id="identifierId"]').send_keys(EMAIL)
+driver.find_element(By.XPATH, '/html/body/div[1]/div[1]/div[2]/c-wiz/div/div[3]/div/div[1]/div/div/button').click()
 
-COMPOSEEMAIL = driver.find_element(By.XPATH, '/html/body/div[7]/div[3]/div/div[2]/div[2]/div[1]/div[1]/div/div')
+sleep(3)
+
+driver.find_element(By.XPATH, '//*[@id="username"]').send_keys(USERNAME)
+driver.find_element(By.XPATH, '//*[@id="password"]').send_keys(PASSWORD)
+driver.find_element(By.XPATH, '//*[@id="loginbtn"]').click()
+
+sleep(5)
+
+# Send email
+with open('mailmerge.csv', 'r') as file:
+	people = [i.strip().split(',') for i in file.readlines()[1:]]
+
+for person in people:
+	email,name = person[:2]
+	workshops = person[2:5]
+	prerequesites = person[6].strip().split('|')
+	ID = person[7]
+	print(email, name, prerequesites)
+	print('\n')
+	# driver.find_element(By.XPATH, '/html/body/div[7]/div[3]/div/div[2]/div[2]/div[1]/div[1]/div/div').click()
---
+++ qrcodeGenerator.py
@@ -0,0 +1,21 @@
+import qrcode
+import json
+
+FOLDER = 'QRCode/'
+PEOPLEFILE = 'participants.json'
+
+with open(PEOPLEFILE, 'r') as file:
+    people = json.load(file)['participants']
+
+for person in people:
+    qr = qrcode.QRCode(
+		version=1,
+		error_correction=qrcode.constants.ERROR_CORRECT_L,
+		box_size=10,
+		border=4,
+	)
+    
+    qr.add_data(person['id'])
+    qr.make(fit=True)
+    img = qr.make_image()
+    img.save(FOLDER + person['id'] + '.png')
